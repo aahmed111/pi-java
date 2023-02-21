eb567266278e737c9dab31c1fd1bcf169e03090e
Ahmed Hmila <ahmed.hmila@esprit.tn>
2023-02-21 21:25:11 +0100

    les crud +jdbc

--- src/edu/workshopjdbc3a48/services/ServiceMessage.java
+++ src/edu/workshopjdbc3a48/services/ServiceMessage.java
@@ -5,10 +5,64 @@
  */
 package edu.workshopjdbc3a48.services;
 
+ 
+import edu.workshopjdbc3a48.entities.Message;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
 /**
  *
  * @author ahmed
  */
-public class ServiceMessage {
+    private int id_message;
+    private Date date ;
+    private String text ;
+    private Chat chat;
+
+
+public class ServiceMessage implements IService<Message> {
+
+    @Override
+    public void ajouter(Message t) {
+        try {
+            String req = "INSERT INTO `Message`(`id_message`, `Date`, `text`, `id_chat`) VALUES (?,NOW(),?,?)";
+            PreparedStatement ps = cnx.prepareStatement(req);
+            ps.setInt(1, t.id_message());
+            ps.setString(2, t.getTitre());
+            ps.setString(3, t.getDescription());
+             ps.setString(4, t.getDate_publication()); 
+             ps.setString(5, t.getStatut()); 
+            ps.executeUpdate();
+            System.out.println("message envoyer !");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }   
+    }
+
+        
+    }
+
+    @Override
+    public void supprimer(int id)   {
+         
+    }
+
+    @Override
+    public void modifier(Message t)   {
+        
+    }
+
+    @Override
+    public List<Message> getAll()   {
+        return null;
+         
+    }
+
+    @Override
+    public Message getOneById(int id)   {
+        return null;
+         
+    }
     
 }
